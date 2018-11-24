7c22b217d858118d5490b5f987dc8d299d06144a
AllegraChen <allegra.chen@mail.utoronto.ca>
2018-11-24 13:45:18 -0500

    Added an App class that is supposed to get the context and let MemoryPuzzleTile to access it. Also added the android:name=".App" into the manifest. Tried to refactor the MemoryPuzzleTile constructor but failed since I the context is always empty

--- Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/MemoryPuzzleTile.java
+++ Phase2/GameCentre/app/src/main/java/fall2018/csc2017/slidingtiles/MemoryPuzzleTile.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 
 import java.io.Serializable;
 
-public class MemoryPuzzleTile implements Comparable<MemoryPuzzleTile>, Serializable {
+public class MemoryPuzzleTile implements Comparable<MemoryPuzzleTile>, Serializable{
 
     /**
      * The background id to find the tile image.
@@ -56,7 +56,25 @@ public class MemoryPuzzleTile implements Comparable<MemoryPuzzleTile>, Serializa
     }
 
 
-    //TODO: refactor this!!!!!!!!!!
+    //TODO: fix this
+
+    /**
+     * A Memory Puzzle tile with a background id, set the id eaquals to the backgroundid
+     *
+     * @param backgroundId the background id of the tile
+     */
+    MemoryPuzzleTile(int backgroundId) {
+        id = backgroundId;
+        String fileName = "memory_tile_" + backgroundId;
+        background = App.getContext().getResources().getIdentifier(
+                fileName, "drawable", "fall2018.csc2017.slidingtiles");
+    }
+
+    public static void main(String[] args) {
+        MemoryPuzzleTile memo = new MemoryPuzzleTile(1);
+        memo.getBackground();
+
+    }
 
     @Override
     public int compareTo(@NonNull MemoryPuzzleTile o) {
@@ -64,3 +82,4 @@ public class MemoryPuzzleTile implements Comparable<MemoryPuzzleTile>, Serializa
         return 0;
     }
 }
+
